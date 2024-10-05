7940c311d9715381d82c773c870a8af8456fa2cb
arjun-saseendran <arjunsaseendran@icloud.com>
2024-10-05 22:46:22 +0530

    add logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,7 +17,7 @@ function Header() {
     <Navbar expand="lg" style={{ background: "#000000" }}>
       <Container>
         <Navbar.Brand href="#home" style={{ color: "#F5F5F7" }}>
-          Shop
+          Black<span>&#128722;</span>Cart
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -53,7 +53,9 @@ function Header() {
       </Container>
       <div className="flex flex-column justify-content-center mx-3">
         <div className="text-white">{count}</div>
-        <div onClick={handleRedirect} style={{cursor: 'pointer'}}>&#128722;</div>
+        <div onClick={handleRedirect} style={{ cursor: "pointer" }}>
+          &#128722;
+        </div>
       </div>
     </Navbar>
   );
